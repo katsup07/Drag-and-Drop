5df822274f4e9bc66bf23afa80f9fbdf1238eae1
katsup <brett_7y7@yahoo.ca>
2022-11-30 20:08:59 +0900

    add html, css, and ts file for rendering form on screen

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("It works!!");
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true); // template node
+        this.element = importedNode.firstElementChild; // form
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element); // add form to main root div
+    }
+}
+const prjInput = new ProjectInput();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log("It works!!");
+class ProjectInput{
+  hostElement: HTMLDivElement;// main root div to output everything
+  templateElement: HTMLTemplateElement; // template that contains form
+  element: HTMLFormElement;// template form
+
+  constructor(){
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true); // template node
+    this.element = importedNode.firstElementChild as HTMLFormElement; // form
+    this.attach();
+  }
+
+  private attach(){
+    this.hostElement.insertAdjacentElement('afterbegin', this.element); // add form to main root div
+  }
+}
+
+const prjInput = new ProjectInput();
